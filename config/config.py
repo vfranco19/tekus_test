91c09190783adc464e221e501e3054927207d997
vfranco19 <victorfranco19@gmail.com>
2026-07-24 20:32:29 -0500

    Remove sensitive test user emails from config
    Removed test user email and signup email from config.

--- config/config.py
+++ config/config.py
@@ -1,8 +1,5 @@
 class Config:
-    """Configuracion centralizada del proyecto.
-
-    Modifica esta clase con los datos de tu aplicacion.
-    """
+    """Configuracion centralizada del proyecto."""
 
     # URL base del sitio a automatizar
     BASE_URL = "https://qalab.invertebrado.co"
@@ -11,9 +8,7 @@ class Config:
     TIMEOUT = 10
 
     # Credenciales de testing
-    TEST_USER_EMAIL = "victor_test_001@prueba.com"
     TEST_USER_PASSWORD = "N9j^u9&Hm@dz2Kcs"
 
     # Datos de registro (ajustar segun el formulario)
     TEST_USER_NAME = "qatester"
-    SIGNUP_EMAIL = "victor_test_003@prueba.com"
